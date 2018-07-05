e29e69bea550df45fb3e6d6a4d105799a85edfbb
codedreamimac <codedreamfy@outlook.com>
2018-07-05 10:46:01 +0800

    rm invalid css

--- src/js/index.js
+++ src/js/index.js
@@ -3,25 +3,3 @@ import utils from "./utils/index";
 
 window.utils = utils;
 window.TimelineLite = TimelineLite;
-
-$(function() {
-  document.addEventListener(
-    "WeixinJSBridgeReady",
-    function() {
-      const video = document.querySelector("video");
-      video.play();
-      video.pause();
-      video.addEventListener("ended", function(e) {
-        // 播放结束时触发
-        if (utils.isiOS()) {
-          globalSwiper.slideTo(2);
-        }
-      });
-    },
-    false
-  );
-  $("img").on("click", function(event) {
-    event.preventDefault();
-    return false;
-  });
-});
